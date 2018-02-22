61919f5522a0d30e03649f23b57a34209419140d
nishashahi <nisha111shahi@gmail.com>
2018-02-22 18:42:18 +0545

    initial commit

---
+++ src/main/java/com/nisha/newstartspring/NewstartspringApplication.java
@@ -0,0 +1,12 @@
+package com.nisha.newstartspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewstartspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewstartspringApplication.class, args);
+	}
+}
